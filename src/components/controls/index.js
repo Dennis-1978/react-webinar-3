96fa12b71e1f87170bcc7bcbc77d030b698978fd
Denis <d.khokhloff2011@yandex.ru>
2023-12-03 10:23:27 +0300

    disabled the activity of the button to go to basket

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
+import { cn as bem } from "@bem-react/classname";
 
 import Total from "../total";
 
 import "./style.css";
 
 function Controls({ count, handleBasketShow, price }) {
+  const cn = bem("Controls");
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    !count
+      ? element.setAttribute("disabled", false)
+      : element.removeAttribute("disabled", null);
+  }, [count]);
+
   return (
-    <div className="Controls">
+    <div className={cn()}>
       <Total count={count} price={price} />
-      <button onClick={() => handleBasketShow()} className="Controls__btn">
+      <button
+        onClick={() => handleBasketShow()}
+        className={cn("btn")}
+        ref={ref}
+      >
         Перейти
       </button>
     </div>
